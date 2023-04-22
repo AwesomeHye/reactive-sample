8ab0dfa4363c6f8a74bf6e171e0eddcbc39858f6
AwesomeHye <45376682+AwesomeHye@users.noreply.github.com>
2023-04-22 15:01:23 +0900

    first init

---
+++ src/main/java/dev/hyein/reactivesample/ReactiveSampleApplication.java
@@ -0,0 +1,81 @@
+package dev.hyein.reactivesample;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.reactivestreams.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+@SpringBootApplication
+@Slf4j
+public class ReactiveSampleApplication {
+
+    /**
+     * 1~10 -> sum -> sub
+     * @param args
+     */
+    public static void main(String[] args) {
+        Publisher<Integer> publisher = new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> subscriber) {
+                Subscription subscription = new Subscription() {
+                    Iterator<Integer> integers = Stream.iterate(1, i -> i + 1).limit(10).collect(Collectors.toList()).iterator();
+
+                    @Override
+                    public void request(long n) {
+                        while (n-- > 0) {
+                            if (integers.hasNext()) {
+                                subscriber.onNext(integers.next());
+                            }
+                        }
+
+                        subscriber.onComplete();
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                };
+                subscriber.onSubscribe(subscription);
+            }
+        };
+
+        Subscriber<Integer> subscriber = new Subscriber<Integer>() {
+            int sum = 0;
+            @Override
+            public void onSubscribe(Subscription s) {
+                s.request(5);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                sum += integer;
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                log.info("complete: {}", sum);
+            }
+        };
+
+        publisher.subscribe(subscriber);
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/dev/hyein/reactivesample/ReactiveSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hyein.reactivesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
